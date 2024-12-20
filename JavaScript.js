86ee8d333301c7402560f844e87858531b05985a
Jorge Koch <jorgeluizkoch@gmail.com>
2024-12-20 01:14:23 -0300

    JS updated, new function added

--- JavaScript.js
+++ JavaScript.js
@@ -1,8 +1,33 @@
 let qtdCartas = 0;
 
 function numeroCartas() {
-   qtdCartas = prompt("Com quantas cartas deseja jogar (escolha um númrero par entre 4 e 14)?")
+   qtdCartas = parseInt(prompt("Com quantas cartas deseja jogar (escolha um númrero par entre 4 e 14)?"));
+   if (qtdCartas < 4 || qtdCartas > 14 || qtdCartas % 2 !== 0){
+    alert("Número inválido!")
+    numeroCartas();
+   }
 }
 
 numeroCartas();
 
+const gifCartas = [];
+
+function adicionarCartas(){
+    const div = document.querySelector(".allCards");
+
+    for(let index = 0; index < qtdCartas; index++) {
+        const elementoCarta = `
+        <div class="card">
+            <div class="front-face face">
+                <img src="./assets/back.png" class="parrot">
+            </div>
+            <div class="back-face face">
+                <img src="./assets/${gifCartas[index]}.gif" class="parrot">
+            </div>
+        </div>
+      `;
+        div.innerHTML += elementoCarta;
+    }
+}
+  
+adicionarCartas();
