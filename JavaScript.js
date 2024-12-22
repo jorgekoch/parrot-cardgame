2642b63307d6f9e9e0ce1d28f12fbcd96f4b22bf
Jorge Koch <jorgeluizkoch@gmail.com>
2024-12-21 23:02:19 -0300

    CSS/JS updated

--- JavaScript.js
+++ JavaScript.js
@@ -1,33 +1,93 @@
 let qtdCartas = 0;
 
 function numeroCartas() {
-   qtdCartas = parseInt(prompt("Com quantas cartas deseja jogar (escolha um númrero par entre 4 e 14)?"));
-   if (qtdCartas < 4 || qtdCartas > 14 || qtdCartas % 2 !== 0){
-    alert("Número inválido!")
-    numeroCartas();
-   }
+    qtdCartas = parseInt(prompt("Com quantas cartas deseja jogar (escolha um número par entre 4 e 14)?"));
+    if (qtdCartas < 4 || qtdCartas > 14 || qtdCartas % 2 !== 0) {
+        alert("Número inválido!");
+        numeroCartas();
+    }
 }
 
 numeroCartas();
 
-const gifCartas = [];
+const gifCartas = [
+    "bobrossparrot.gif",
+    "explodyparrot.gif",
+    "fiestaparrot.gif",
+    "metalparrot.gif",
+    "revertitparrot.gif",
+    "tripletsparrot.gif",
+    "unicornparrot.gif"
+];
 
-function adicionarCartas(){
+function embaralhar() {
+    return Math.random() - 0.5;
+}
+
+function adicionarCartas() {
     const div = document.querySelector(".allCards");
 
-    for(let index = 0; index < qtdCartas; index++) {
+    let cartasEscolhidas = gifCartas.slice(0, qtdCartas / 2);
+    cartasEscolhidas = [...cartasEscolhidas, ...cartasEscolhidas];
+
+    cartasEscolhidas.sort(embaralhar);
+
+    cartasEscolhidas.forEach((gif) => {
         const elementoCarta = `
-        <div class="card">
+        <div class="card" onclick="virarCarta(this)" data-gif="${gif}">
             <div class="front-face face">
                 <img src="./assets/back.png" class="parrot">
             </div>
             <div class="back-face face">
-                <img src="./assets/${gifCartas[index]}.gif" class="parrot">
+                <img src="./assets/${gif}" class="parrot">
             </div>
         </div>
-      `;
+        `;
         div.innerHTML += elementoCarta;
+    });
+}
+
+adicionarCartas();
+
+let primeiraCarta = null;
+let segundaCarta = null;
+let bloqueio = false;
+
+function virarCarta(carta) {
+    if (bloqueio || carta.classList.contains('flipped') || carta.classList.contains('matched')) {
+        return;
+    }
+
+    carta.classList.add('flipped');
+
+    if (!primeiraCarta) {
+        primeiraCarta = carta;
+        return;
+    }
+
+    segundaCarta = carta;
+    verificarPar();
+}
+
+function verificarPar() {
+    const gifPrimeira = primeiraCarta.getAttribute('data-gif');
+    const gifSegunda = segundaCarta.getAttribute('data-gif');
+
+    if (gifPrimeira === gifSegunda) {
+        primeiraCarta.classList.add('matched');
+        segundaCarta.classList.add('matched');
+        resetarJogada();
+    } else {
+        bloqueio = true;
+        setTimeout(() => {
+            primeiraCarta.classList.remove('flipped');
+            segundaCarta.classList.remove('flipped');
+            resetarJogada();
+        }, 1000);
     }
 }
-  
-adicionarCartas();
+
+function resetarJogada() {
+    [primeiraCarta, segundaCarta] = [null, null];
+    bloqueio = false;
+}
